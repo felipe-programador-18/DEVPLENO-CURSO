9e1e07168fa7ac81a11830efef764fddb7debfb4
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-28 16:46:35 -0300

    c02-  adding and practice more filter and reducer

--- ALL.js/tradingnew.functions.js
+++ ALL.js/tradingnew.functions.js
@@ -28,3 +28,20 @@ const Sure = (Mother,mae) => {
 
 const Union = Moresure.reduce(Sure, {})
 console.log(Union)
+
+
+
+const referral = function(item){
+    return item < 50
+}
+
+const createnew = [12,3,123,54,6,5,49,43,32].filter(referral)
+console.log('teste array and filter!!!', createnew)
+
+//make filter to people born below of year 2000
+const Decade = function(born){
+    return born < 2000 && born > 1975
+}
+
+let Vector = [1995,1996,1999,1976, 1920, 1930].filter(Decade)
+console.log(Vector)
