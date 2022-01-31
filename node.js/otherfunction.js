cbea46c162a8f5ae1bbcdff4ba6d15cfb32fb434
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-31 10:22:32 -0300

    c3- adding new folder about file

---
+++ node.js/otherfunction.js
@@ -0,0 +1,7 @@
+const Append = (c,d)  => c * d
+const Equal = Append(10,600)
+console.log(Equal)
+
+module.export = {
+    Equal
+}
---
+++ node.js/pacote.js
@@ -0,0 +1,8 @@
+const Sum = (a,b)=> a+b
+const Together = Sum(10,20)
+
+console.log(Together)
+
+module.export = {
+    Together
+}
